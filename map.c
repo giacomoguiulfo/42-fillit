c13b1c1c5d6771c9a6f45ef0e18ad013796c32d6
Jibran Kalia <jkalia@e1z2r9p33.42.us.org>
2017-03-09 14:48:14 -0800

    Initial work on the recursion

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/08 09:44:19 by jkalia            #+#    #+#             */
-/*   Updated: 2017/03/09 14:20:10 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/03/09 14:47:56 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -221,10 +221,23 @@ int		test_place(char **tbl, size_t blocks)
 
 int		solve(char **tbl, size_t blocks)
 {
-	//char	**map;
+	char	**map;
+	size_t	map_size;
+	//test_place(tbl, blocks);
 
-	test_place(tbl, blocks);
-	//CHK1((map = new_map(blocks)) == 0, ft_putstr("Error in Map Allocation\n"), 0);
+	map_size = initial_board_size(blocks);
+	CHK1((map = new_map(map_size)) == 0, ft_putstr("Error in Map Allocation\n"), 0);
+	while (recursion(tbl, map, 0, 0) == false)
+	{
+		map_size++;
+		delete_map(map);
+		CHK1((map = new_map(map_size)) == 0, ft_putstr("Error in Map Allocation\n"), 0);
+		recursion(tbl, map, 0, 0);
+		
+	}
+	print_map(map, blocks);
+	delete_map(map);
+
 	/**
 	if (place(map, tbl[4], 26, 0))
 		printf("Placed properly\n");
@@ -234,8 +247,25 @@ int		solve(char **tbl, size_t blocks)
 		printf("Placed properly\n");
 	else
 		printf("NOT placed properly\n");
-	print_map(map, blocks);
 	**/
-//	delete_map(map);
 	return (0);
+}
+
+
+
+
+t_bool	recursion(char **tbl, char **map, int row, int col)
+{
+	while (map[row])
+	{
+		while (map[row][col])
+		{
+			if (place(map, *tbl, col, row) == true)
+				solver(tbl + 1, map, 0, 0);
+			col++;
+		}
+		row++;
+	}
+
+
 }
