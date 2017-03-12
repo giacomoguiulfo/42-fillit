a9ae252db74e9e96c6046b24f8534d1abaf7ff3c
Jibran Kalia <jkalia@e1z2r7p10.42.us.org>
2017-03-12 11:21:54 -0700

    Moved bool type to libft

--- fillit.h
+++ fillit.h
@@ -6,28 +6,22 @@
 /*   By: jkalia <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/05 12:03:05 by jkalia            #+#    #+#             */
-/*   Updated: 2017/03/11 10:19:16 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/03/12 11:21:08 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
-
-# include "libft.h"
 # include <stdlib.h>
+# include <libft.h>
+
 # define BUFFER_SIZE 546
 # define V1(a) (a == '#' || a == '.')
 # define V2(a) (a == '\n')
 # define V3(a) (a == '#' || a == '.' || a == '\n')
 # define DOT(a) (a == '.')
-# define DO2(a, b) do{a; b;}while(0)
-# define DO3(a, b, c) do{a; b; c;}while(0)
-
-typedef enum	e_bool
-{
-	false,
-	true
-}				t_bool;
+# define DO2(a, b) if(1){a; b;}
+# define DO3(a, b, c) if(1){a; b; c;}
 
 void			error(void);
 void			change_end(char **src, int bytes);
